64046be2c52185e8d13319880a65e45cfd18d5d9
Rajbanshi <60051485+ihsnabjar@users.noreply.github.com>
2021-09-14 23:18:52 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   reactStrictMode: false,
   env: {
-    GITHUB_ID:'fca8fac6f72138a6508c',
-    GITHUB_SECRET: '2a76f2f7a0a30ae51623a0228cdc8a8be08b09a3',
+    GITHUB_ID:'your github id',
+    GITHUB_SECRET: 'github secret',
     NEXTAUTH_URL: 'http://localhost:3000',
     JWT_SECRET:'dfhghjdfgjfdfjhdgfjhdbfjhdbfjhdsbfjdsbf',
-    MONGODB_URI_S:'mongodb+srv://rajbanshi:rajbanshi12@cluster0.e6neo.gcp.mongodb.net/nextAuthDb?retryWrites=true&w=majority',
     MONGODB_URI:'mongodb://localhost:27017',
   },
   // images: {
